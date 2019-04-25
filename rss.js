d4f881de0c9a0ebc65c0720d2e018e3c79f15a93
Ryan Streur <rjstreur@gmail.com>
2019-04-24 20:33:34 -0700

    add command for searching and opening saved items

--- rss.js
+++ rss.js
@@ -1,24 +1,25 @@
 #!/usr/bin/env node
-const program = require('commander');
-const getCreatorCounts = require('./lib/item-list').getCreatorCounts;
-const pull = require('./lib/feed-single').pull;
-const getConfig = require('./lib/config').getConfig;
-const commands = require('./lib/commands');
+const program = require("commander");
+const getCreatorCounts = require("./lib/item-list").getCreatorCounts;
+const pull = require("./lib/feed-single").pull;
+const getConfig = require("./lib/config").getConfig;
+const commands = require("./lib/commands");
 
-program.version('0.0.1');
-program.command('add-feed <feedUrl>').action(commands.addFeedCmd);
+program.version("0.0.1");
+program.command("add-feed <feedUrl>").action(commands.addFeedCmd);
 program
-  .command('pull-items [feedUrl]')
-  .option('-hl, --headlines')
+  .command("pull-items [feedUrl]")
+  .option("-hl, --headlines")
   .action(commands.pullItems);
 
 program
-  .command('read')
-  .option('-s, --saved')
+  .command("read")
+  .option("-s, --saved")
   .action(commands.read);
 
-program.command('feeds').action(commands.feeds);
+program.command("feeds").action(commands.feeds);
+program.command("search-saved").action(commands.searchSaved);
 
-program.command('feed', 'manage rss feeds');
+program.command("feed", "manage rss feeds");
 
 program.parse(process.argv);
